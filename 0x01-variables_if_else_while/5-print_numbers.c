f78835d127aa3784fecc35a994eb1575d14c093b
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-09 03:00:32 -0700

    Print those digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print 0-9
+ * Return: (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
