5d5cc1823b4409279e6df5e91e7863d5bd45e986
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-12 13:09:29 -0700

    Syntax

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char[] dora = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char dora[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, dora, sizeof(dora));
 	return (1);
