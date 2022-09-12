0e5720d1b35c360743661a218b2e3dc2b4a2e7c1
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-12 14:38:09 -0700

    Two groups

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - combos of two print
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					if (((k + l) > (i + j) && k >= i) || i < k)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+
+					if (i + j + k + l == 227 && i == 57)
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
