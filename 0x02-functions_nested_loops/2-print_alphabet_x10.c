2fa0911ff145324526f8cb716358a49dd59f164b
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-14 03:33:52 -0700

    Alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		putchar('\n');
+	}
+}
