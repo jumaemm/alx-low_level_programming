6246274a4342925283b044fb617e72b2147d2ee6
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-19 03:50:54 -0700

    Reverse string in place

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to be reversed
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	int size;
+	char tempChar;
+
+	/*Evaluate length*/
+	for (size = 0; s[size] != '\0'; size++)
+	{
+	}
+
+	j = size - 1;
+	i = 0;
+
+	while (j < i)
+	{
+		temp1 = s[i];
+		s[i] = s[j];
+		s[j] = tempChar;
+		j--;
+		i++;
+	}
+}
