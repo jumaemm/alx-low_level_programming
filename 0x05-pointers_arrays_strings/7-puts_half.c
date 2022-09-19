934b0090b53a411a07d6bc3da668f0fa398ca074
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-19 04:12:10 -0700

    Puthalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - Print half a string
+ * @str: String to be printed
+ * Return: Always 0
+ */
+void puts_half(char *str)
+{
+	int i, size, half;
+
+	for (size = 0; str[size] != '\n'; size++)
+	{
+	}
+	if (size % 2 == 0)
+	{
+		half = size / 2;
+	}
+	else
+	{
+		half = (size - 1) / 2;
+	}
+	for (i = half; i < size; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
