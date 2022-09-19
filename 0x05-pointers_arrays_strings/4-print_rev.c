a979f6db8fb38ce8d2d23c681a9fe03f2aca89a5
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-19 02:08:16 -0700

    Print reverse string test run with length

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print reverse of given string
+ * @s: string to be checked
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+	int size;
+
+	/* find length of the string first*/
+	size = _strlen(*s);
+
+	/* iterate reversely over the string */
+	for (i = size - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
