ad9b6df47d1b5b1d5935ef1311a5548f310535ac
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-19 04:31:57 -0700

    Print an array to a particular point

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n chars of array
+ * @a: Array to be printed
+ * @n: number of chars
+ * Return: Always 0
+ */
+void print_array(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
