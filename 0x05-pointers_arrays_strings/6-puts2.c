8ff4a95572bbbfe23c26187c6a5c3d8ac3cdb367
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-19 04:00:22 -0700

    Puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Print every other char in string
+ * @str: String to be checked
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
