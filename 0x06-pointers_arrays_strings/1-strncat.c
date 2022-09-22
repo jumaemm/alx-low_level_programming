ea40feea38e455cbd5a412fb495bb9153e9cb537
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-22 03:47:13 -0700

    Concatenate particular bytes of src string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,5 +10,24 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0' && n > j)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	if (n > 0)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
 }
