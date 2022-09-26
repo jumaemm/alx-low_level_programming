297f95856be8c58f4f3af724b9e54731467aa38f
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-26 03:19:24 -0700

    Print sum of the diagonals of a multidimensional square array

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagsums - print sum of two diagonals of matrix
+ * @a: input array
+ * @size: size of the array
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, sum_left = 0, sum_right = 0;
+
+	/*Multi-dimensional arrays are stored linearly in memory*/
+	for (i = 0, j = size - 1; i < (size * size); i += size + 1, j += size - 1)
+	{
+		sum_left += a[i];
+		sum_right += a[j];
+	}
+	printf("%d, %d", sum_left, sum_right);
+}
