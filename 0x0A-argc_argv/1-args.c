c7f387b20c0aac218849abe23c2e4cf1db828b4b
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-09-30 00:37:40 -0700

    Return count f args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print number of args
+ * @argc: count of args
+ * @argv: array of all args
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
