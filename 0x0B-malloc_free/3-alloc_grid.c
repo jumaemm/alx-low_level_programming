ebf0ded84e10dad8fbe462962c7439d8ea560517
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-04 04:04:00 -0700

    Return actual 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * **alloc_grid - returns pointer to 2D array of ints
@@ -8,18 +9,36 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int *arr, i = 0, j = 0;
+	int **arr, i = 0, j = 0, k = 0, flag_check = 0;
 
 	if (width <= 0 || height <= 0)
 		return (0);
 
-	arr = malloc(sizeof(int) * (width * height));
+	arr = malloc(sizeof(int) * height);
 	if (arr == 0)
 		return (0);
 
-	for (; i  < (width * height); i++)
+	for (; i  < height; i++)
 	{
-		arr[i] = 0;
+		*(arr + i) = malloc(sizeof(int) * width);
+		if (*(arr + i) == 0)
+		{
+			flag_check = 1;
+			break;
+		}
+		for (; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	if (flag_check == 1)
+	{
+		for (; k < i; k++)
+		{
+			free(*(arr + k));
+		}
+		free(arr);
 	}
 	return (arr);
 }
