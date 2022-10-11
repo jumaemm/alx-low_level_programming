14c406bdb7cd69c17b56effc21c0d2578231a9e3
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-11 01:45:43 -0700

    Print my dog's name

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints struct dog
+ * @d: dog struct
+ * Return: Always 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d != 0)
+	{
+		if ((*d).name != 0)
+		{
+			printf("Name: %s\n", (*d).name);
+		}
+		else
+			printf("Name: (nil)\n");
+		if ((*d).age != 0)
+			printf("Age: %f\n", (*d).age);
+		else
+			printf("Age: (nil)\n");
+		if ((*d).owner != 0)
+		{
+			printf("Owner: %s\n", (*d).owner);
+		}
+		else
+			printf("Owner: (nil)\n");
+	}
+}
