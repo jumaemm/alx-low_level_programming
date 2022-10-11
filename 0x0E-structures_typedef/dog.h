6ab21f61da63f4d01e2ac77718bb71b64d05c1ae
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-11 01:30:46 -0700

    Dog header file protected

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef STRUCTS
+#define STRUCTS
+
+/**
+ * struct dog - dog data structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: oener of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
+
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
