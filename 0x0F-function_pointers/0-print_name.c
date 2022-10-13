e97316ca89a56248a0bf88a1cbb3d0995a71f724
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-13 13:07:40 -0700

    Print name using func pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints given name using given func
+ * @name: name to be printed
+ * @f: pointer to function
+ * Return: Always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
