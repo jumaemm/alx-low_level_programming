af5155a03228bf62f79b0b121220189867be0958
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-14 02:58:20 -0700

    Summing up args using variadic functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <variadic_functions.h>
+
+/**
+ * sum_them_all - sum up all args given
+ * @n: first arg
+ * Return: sum of args or 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	va_start(ap, n);
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (sum);
+}
