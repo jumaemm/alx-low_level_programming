547177a2de5f84316cad3f4126feee10910d5ae4
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-14 03:21:50 -0700

    Count zero check

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,6 +15,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
+	if (n == 0)
+	{
+		return (0);
+	}
 	if (separator != NULL)
 	{
 		for (i = 0; i < n; i++)
