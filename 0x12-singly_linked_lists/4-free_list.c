eaed7986a66a46f33eafdd9846f23b1d1dce1afe
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-24 13:12:18 -0700

    Free memory allocated to linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free memory allocated to list
+ * @head: head of singly linked list
+ * Return: Always 0
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	free_list(head->next);
+	free(head->str);
+	free(head->len);
+	free(head);
+}
