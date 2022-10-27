7e7a65a7b60386ff09cc541b006ca12a8e7e4c39
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-27 12:59:23 -0700

    Iterate through current_node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current_node = *head;
 	listint_t *new_node = malloc(sizeof(listint_t));
 
-	if (!*new_node)
+	if (!new_node)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
@@ -37,6 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (new_node);
 		}
 		position += 1;
+		current_node = current_node->next;
 	}
 	free(new_node);
 	return (NULL);
