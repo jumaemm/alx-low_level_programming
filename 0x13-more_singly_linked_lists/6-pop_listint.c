b641693f7b48b42d177bca4334d5d6f58ddd3ada
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-27 03:48:01 -0700

    Pop the head of a linked list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * pop_listint - delete the head of linked list
+ * @head: head of the linked list
+ * Return: valur of node or 0
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp_node = head;
+	int node_value;
+
+	if (*head == NULL)
+		return (0);
+	node_value = *head->n;
+	*temp_node = *head;
+	*head = temp_node->next;
+	free(temp_node);
+	return (node_value);
+}
