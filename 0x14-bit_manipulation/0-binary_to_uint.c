9fb4ddf7b4b028a97d5d9ff222e40249f2ce606f
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-10-31 14:27:09 -0700

    NULL check

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int result = 0;
 	int length = 0, power;
 
-	if (b[length] == '\0')
+	if (b[length] == '\0' || b == NULL)
 	{
 		return (0);
 	}
