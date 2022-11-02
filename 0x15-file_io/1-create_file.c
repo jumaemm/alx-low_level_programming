2c88205a530f628ceecd322cac8d074c2878aa9a
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-02 14:36:50 -0700

    Write variable check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	wr = write(fd, text_content, length);
-	if (fd == -1)
+	if (wr == -1)
 		return (-1);
 	close(fd);
 	return (1);
