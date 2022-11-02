76a018a554124b016799cabcf759271bf63d0f2b
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-02 14:51:41 -0700

    betty refactor

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 /**
  * append_text_to_file - append text to end of file
  * @filename: name of the file
- * text_content: text to append
+ * @text_content: text to append
  * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
