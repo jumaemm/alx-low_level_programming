f39351df567ce48f089b6c6a3b8bc57ce3875135
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-02 14:10:01 -0700

    Refactor

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,18 +11,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t fd, rd, wr;
 	char *buff;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
 	buff = malloc(sizeof(char) * letters);
-	if (!buff)
+	if (buff == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
 	rd = read(fd, buff, letters);
 	wr = write(1, buff, rd);
 
-	if (fd == -1 || rd == -1 || wr == -1 || wr != rd)
+	if (rd == -1 || wr == -1 || wr != rd)
 	{
 		free(buff);
 		return (0);
