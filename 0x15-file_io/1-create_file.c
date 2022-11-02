ad0d166c3c47a9c961d9e62bc8b16d6381556891
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-02 14:24:52 -0700

    Create a file and assign perms

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - create text file with givencontent
+ * @filename: name of the file
+ * @text_content: string to write to file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wr, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[length])
+			length++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	wr = write(fd, text_content, length);
+	if (fd == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
