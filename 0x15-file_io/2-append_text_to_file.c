3e82046025f2637ace0990b881d53191a4761753
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-02 14:50:01 -0700

    Append text to end of file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - append text to end of file
+ * @filename: name of the file
+ * text_content: text to append
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wr, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[length])
+			length++;
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	wr = write(fd, text_content, length);
+	if (fd == -1 || wr == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
