a3ef0deedc5d5daae7dc89cdb8dd85c1f0d6c769
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-08 11:57:13 +0000

    Get node at particular index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - return nth node of doubly linked list
+ * @head: head of linked list
+ * @index: index of node
+ * Return: NULL otherwise address of node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int node_count = 0;
+	dlistint_t *current;
+
+	if (head == NULL)
+		return (NULL);
+	current = head;
+	while (node_count <= index)
+	{
+		if (node_count == index)
+			return (current);
+		node_count++;
+		current = current->next;
+	}
+	return (NULL);
+}
