9d44de79ff03f7a0cbe3ee0d5788f9ae048387cc
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-12-08 11:44:07 +0000

    Free resources alloced to doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free a dlistint list
+ * @head: head of linked list
+ * Return: Always 0
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t temp_node;
+
+	while (head)
+	{
+		temp_node = head->next;
+		free(head);
+		head = temp_node;
+	}
+}
