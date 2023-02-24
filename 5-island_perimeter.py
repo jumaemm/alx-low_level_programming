879d27785aa38e5184d3b6909951176c8909d138
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2023-02-24 19:59:50 +0000

    perimeter of an island in a grid

---
+++ 5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""This module calculates the perimeter of an island
+    represented in a grid
+"""
+
+
+def island_perimeter(grid):
+    """Calculate the perimeter of a given grid.
+    Arguments:
+        grid(list): grid representing an island and water
+    Return:
+        perimter of the island
+    """
+    land = 0
+    edges = 0
+    width = len(grid[0])
+    height = len(grid)
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                # Island located
+                land += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+    return (land * 4) - (edges * 2)
